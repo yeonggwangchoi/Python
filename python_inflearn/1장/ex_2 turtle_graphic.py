86d18eabe7674333512417df33bed397d58f85ee
yeonggwangchoi <dudrhkd7811@naver.com>
2022-02-16 10:19:52 +0900

    220216 first commit

---
+++ python_inflearn/1장/ex_2 turtle_graphic.py
@@ -0,0 +1,20 @@
+##python turtle module을 가져온다.
+import turtle as t
+
+t.shape("circle")
+
+t.showturtle()
+
+t.write("Welcome to Python")
+
+##직선으로 100픽셀 만큼 그려라.
+t.forward(100)
+t.right(90)
+t.color("red")
+t.forward(100)
+t.right(90)
+t.color("yellow")
+t.forward(100)
+t.right(90)
+t.color("green")
+t.forward(100)
---
+++ python_inflearn/1장/idle tutle.py
@@ -0,0 +1,10 @@
+##python turtle module을 가져온다.
+import turtle
+
+t=turtle
+
+t.showturtle()
+t.write("Welcome to Python")
+
+##직선으로 100픽셀 만큼 그려라.
+t.forward(100)
