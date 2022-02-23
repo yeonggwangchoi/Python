819991a87ee64b92d073aba7da56398785032e0f
yeonggwangchoi <dudrhkd7811@naver.com>
2022-02-23 21:34:49 +0900

    220223 python study commit

---
+++ python_inflearn/python 6시간 뒤면 나도 개발자/연산자/Quiz.py
@@ -0,0 +1,4 @@
+from random import *
+
+
+print("오프라인 스터디 모임 날짜는 매월 ",randint(4,28),"일로 선정되었습니다.")
---
+++ python_inflearn/python 6시간 뒤면 나도 개발자/연산자/ex04.py
@@ -0,0 +1,27 @@
+from random import *
+
+print(random())#0.0~1.0미만의 임의의 값 생성
+print(random()*10)#0.0~10.0미만의 임의의 값 생성
+print(int(random()*10))#0.0~10.0미만의 임의의 값 생성
+print(int(random()*10)+1)#0.0~10.0미만의 임의의 값 생성
+print(int(random()*10)+1)#0.0~10.0미만의 임의의 값 생성
+print(int(random()*10)+1)#0.0~10.0미만의 임의의 값 생성
+print(int(random()*10)+1)#0.0~10.0미만의 임의의 값 생성
+print(int(random()*10)+1)#0.0~10.0미만의 임의의 값 생성
+print(int(random()*10)+1)#0.0~10.0미만의 임의의 값 생성
+print(int(random()*10)+1)#0.0~10.0미만의 임의의 값 생성
+print(" ")
+print(int(random()*45)+1)#0.0~10.0미만의 임의의 값 생성
+print(int(random()*45)+1)#0.0~10.0미만의 임의의 값 생성
+print(int(random()*45)+1)#0.0~10.0미만의 임의의 값 생성
+print(int(random()*45)+1)#0.0~10.0미만의 임의의 값 생성
+print(int(random()*45)+1)#0.0~10.0미만의 임의의 값 생성
+print(int(random()*45)+1)#0.0~10.0미만의 임의의 값 생성
+print(" ")
+print(randrange(1,46)) #1부터 46미만의 임의의 값 생성
+print(randrange(1,46)) #1부터 46미만의 임의의 값 생성
+print(randrange(1,46)) #1부터 46미만의 임의의 값 생성
+print(randrange(1,46)) #1부터 46미만의 임의의 값 생성
+print(randrange(1,46)) #1부터 46미만의 임의의 값 생성
+print(randrange(1,46)) #1부터 46미만의 임의의 값 생성
+
