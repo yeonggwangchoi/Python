7c6a5758e1cefb487f5f60cbab1c6c299e4626bb
yeonggwangchoi <dudrhkd7811@naver.com>
2022-06-21 15:42:23 +0900

    220621 commit

---
+++ 혼자 공부하는 파이썬/2장/input.py
@@ -0,0 +1,2 @@
+string = input("인사말을 입력하세요> ")
+print(string)
---
+++ 혼자 공부하는 파이썬/2장/int_float02.py
@@ -0,0 +1,7 @@
+a=float(input("입력 A > "))
+b=float(input("입력 B > "))
+
+print("덧셈 결과 : ",a+b)
+print("뺄셈 결과 : ",a-b)
+print("곱셈 결과 : ",a*b)
+print("나눗셈 결과 : ",a/b)
