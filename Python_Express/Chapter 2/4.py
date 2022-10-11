daefc093fe6532150d1fda2b7b61c0639a3571f2
yeonggwangchoi <dudrhkd7811@naver.com>
2022-10-11 23:07:35 +0900

    221011 commit

---
+++ Python_Express/Chapter 2/4.py
@@ -0,0 +1,5 @@
+hour = int(input("시간을 입력하세요: "))
+min = int(input("분을 입력하세요: "))
+sec = hour * 3600+ min * 60
+
+print(hour, "시간", min, "분은", sec, "초입니다.")
--- inflearn_image_processing/image_open.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import sys
-img = cv2.imread('cat.jpg')
-
-cv2.imshow('img', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
